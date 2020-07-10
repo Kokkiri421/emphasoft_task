db7a8c04b1ca2720eb10951f15d5770fe33d9a9f
Fuuraiken83 <Fuuraiken83@github.com>
2020-07-11 00:24:00 +0300

    heroku deployment with instance v2

--- instance/config.py
+++ instance/config.py
@@ -5,6 +5,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 CLIENT_ID = '7535324'
-REDIRECT_URI = 'https://emphasofttask.herokuapp.com/'
+REDIRECT_URI = 'https://emphasoft-vk-task.herokuapp.com/'
 CLIENT_SECRET = 'v4jIc0UsD3SUf4sfovZR'
 
